e7b64e91ec85ef1734ec90bf769cf9f3f402da57
YelyzavetaMala <malaelizaveta678@gmail.com>
2023-12-14 23:01:06 +0200

    first task

---
+++ js/task-01.js
@@ -0,0 +1,10 @@
+const categoriesList = document.getElementById('categories');
+const categoriesItems = categoriesList.querySelectorAll('li.item');
+console.log('Number of categories: ${categoriesItems.length}');
+
+categoriesItems.forEach(category => {
+  const categoryName = category.querySelector('h2').textContent;
+  const categoryItemsCount = category.querySelectorAll('ul li').length;
+    console.log('Category: ${categoryName}');
+    console.log('Elements: ${categoryItemsCount}');
+});
