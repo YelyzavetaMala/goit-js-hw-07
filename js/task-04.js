3f157c71aa2d8fe006a4f3cbc8aa30a913edd393
YelyzavetaMala <malaelizaveta678@gmail.com>
2023-12-17 10:43:07 +0200

    correction

--- js/task-04.js
+++ js/task-04.js
@@ -1,4 +1,4 @@
-const loginForm = document.querySelector('.loginForm');
+const loginForm = document.querySelector('.login-form');
 
     loginForm.addEventListener('submit', function (event) {
       event.preventDefault();
